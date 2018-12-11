mgedmin/check-python-versions

commit 2e4a76e901593d2cc0bf89539cfc548ffc14ea45

Refactoring, tests

check_python_versions.py
@@ -106,16 +106,22 @@ def get_setup_py_keyword(setup_py, keyword):
         except SyntaxError as error:
             warn(f'Could not parse {setup_py}: {error}')
             return None
+    node = find_call_kwarg_in_ast(tree, 'setup', keyword)
+    if node is None:
+        warn('Could not find setup() call in setup.py')
+        return None
+    return eval_ast_node(node, keyword)
+
+
+def find_call_kwarg_in_ast(tree, funcname, keyword):
     for node in ast.walk(tree):
         if (isinstance(node, ast.Call)
                 and isinstance(node.func, ast.Name)
-                and node.func.id == 'setup'):
+                and node.func.id == funcname):
             for kwarg in node.keywords:
                 if kwarg.arg == keyword:
-                    return eval_ast_node(kwarg.value, keyword)
+                    return kwarg.value
             break
-    else:
-        warn('Could not find setup() call in setup.py')
     return None
 
 

tests.py
@@ -1,3 +1,5 @@
+import ast
+
 import pytest
 
 import check_python_versions as cpv
@@ -38,6 +40,28 @@ def test_get_versions_from_classifiers_with_trailing_whitespace():
     assert cpv.get_versions_from_classifiers([
         'Programming Language :: Python :: 3.6 ',
     ]) == ['3.6']
+
+
+def test_find_call_kwarg_in_ast():
+    tree = ast.parse('foo(bar="foo")')
+    ast.dump(tree)
+    node = cpv.find_call_kwarg_in_ast(tree, 'foo', 'bar')
+    assert isinstance(node, ast.Str)
+    assert node.s == "foo"
+
+
+@pytest.mark.parametrize('code, expected', [
+    ('"hi"', "hi"),
+    ('"hi\\n"', "hi\n"),
+    ('["a", "b"]', ["a", "b"]),
+    ('("a", "b")', ("a", "b")),
+    ('"-".join(["a", "b"])', "a-b"),
+])
+def test_eval_ast_node(code, expected):
+    tree = ast.parse(f'foo(bar={code})')
+    node = cpv.find_call_kwarg_in_ast(tree, 'foo', 'bar')
+    assert node is not None
+    assert cpv.eval_ast_node(node, 'bar') == expected
 
 
 def test_parse_python_requires_empty():

commit 35269623c9f4e9786f341f6a794a88fddb16b192

Fix wrong error for setup() w/o python_requires=
Fixes: 2e4a76e901593d2cc0bf89539cfc548ffc14ea45

check_python_versions.py
@@ -107,13 +107,10 @@ def get_setup_py_keyword(setup_py, keyword):
             warn(f'Could not parse {setup_py}: {error}')
             return None
     node = find_call_kwarg_in_ast(tree, 'setup', keyword)
-    if node is None:
-        warn('Could not find setup() call in setup.py')
-        return None
-    return eval_ast_node(node, keyword)
+    return node and eval_ast_node(node, keyword)
 
 
-def find_call_kwarg_in_ast(tree, funcname, keyword):
+def find_call_kwarg_in_ast(tree, funcname, keyword, filename='setup.py'):
     for node in ast.walk(tree):
         if (isinstance(node, ast.Call)
                 and isinstance(node.func, ast.Name)
@@ -121,8 +118,11 @@ def find_call_kwarg_in_ast(tree, funcname, keyword):
             for kwarg in node.keywords:
                 if kwarg.arg == keyword:
                     return kwarg.value
-            break
-    return None
+            else:
+                return None
+    else:
+        warn(f'Could not find {funcname}() call in {filename}')
+        return None
 
 
 def eval_ast_node(node, keyword):

tests.py
@@ -89,7 +89,7 @@ def test_get_python_requires(tmp_path, monkeypatch):
     assert cpv.get_python_requires(setup_py) == ['3.6', '3.7']
 
 
-def test_get_python_requires_not_specified(tmp_path):
+def test_get_python_requires_not_specified(tmp_path, capsys):
     setup_py = tmp_path / "setup.py"
     setup_py.write_text(textwrap.dedent("""\
         from setuptools import setup
@@ -98,6 +98,7 @@ def test_get_python_requires_not_specified(tmp_path):
         )
     """))
     assert cpv.get_python_requires(setup_py) is None
+    assert capsys.readouterr().err == ''
 
 
 def test_get_setup_py_keyword_syntax_error(tmp_path, capsys):
@@ -118,6 +119,22 @@ def test_find_call_kwarg_in_ast():
     node = cpv.find_call_kwarg_in_ast(tree, 'foo', 'bar')
     assert isinstance(node, ast.Str)
     assert node.s == "foo"
+
+
+def test_find_call_kwarg_in_ast_no_arg(capsys):
+    tree = ast.parse('foo(baz="foo")')
+    ast.dump(tree)
+    node = cpv.find_call_kwarg_in_ast(tree, 'foo', 'bar')
+    assert node is None
+    assert capsys.readouterr().err == ''
+
+
+def test_find_call_kwarg_in_ast_no_call(capsys):
+    tree = ast.parse('fooo(bar="foo")')
+    ast.dump(tree)
+    node = cpv.find_call_kwarg_in_ast(tree, 'foo', 'bar')
+    assert node is None
+    assert 'Could not find foo() call in setup.py' in capsys.readouterr().err
 
 
 @pytest.mark.parametrize('code, expected', [