d3ac98920dcca959bf93ecd61b006fadbebf3c91
Marius Gedminas <marius@gedmin.as>
2018-12-11 18:25:54 +0200

    And more simple tests

--- tests.py
+++ tests.py
@@ -365,6 +365,36 @@ def test_appveyor_normalize_py_version(s, expected):
     assert cpv.appveyor_normalize_py_version(s) == expected
 
 
+def test_get_manylinux_python_versions(tmp_path):
+    manylinux_install_sh = tmp_path / ".manylinux-install.sh"
+    manylinux_install_sh.write_text(textwrap.dedent(r"""
+        #!/usr/bin/env bash
+
+        set -e -x
+
+        # Compile wheels
+        for PYBIN in /opt/python/*/bin; do
+            if [[ "${PYBIN}" == *"cp27"* ]] || \
+               [[ "${PYBIN}" == *"cp34"* ]] || \
+               [[ "${PYBIN}" == *"cp35"* ]] || \
+               [[ "${PYBIN}" == *"cp36"* ]] || \
+               [[ "${PYBIN}" == *"cp37"* ]]; then
+                "${PYBIN}/pip" install -e /io/
+                "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+                   rm -rf /io/build /io/*.egg-info
+            fi
+        done
+
+        # Bundle external shared libraries into the wheels
+        for whl in wheelhouse/zope.interface*.whl; do
+            auditwheel repair "$whl" -w /io/wheelhouse/
+        done
+    """.lstrip('\n')))
+    assert cpv.get_manylinux_python_versions(manylinux_install_sh) == [
+        '2.7', '3.4', '3.5', '3.6', '3.7',
+    ]
+
+
 def test_important():
     assert cpv.important({
         '2.7', '3.4', '3.7-dev', 'nightly', 'PyPy3', 'Jython'
